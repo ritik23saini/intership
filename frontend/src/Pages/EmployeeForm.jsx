e32fa32f3945feb3aa056356daf7d969290e4446
ritik23saini <ritik23saini@gmail.com>
2025-06-02 05:31:50 +0530

    last

--- frontend/src/Pages/EmployeeForm.jsx
+++ frontend/src/Pages/EmployeeForm.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
-import { Form, Link } from "react-router-dom";
+import {  Link } from "react-router-dom";
 
 const EmployeeForm = () => {
     const initialState = {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
-} from "react-router";
+} from "react-router-dom";
 import App from './App';
 import Dashboard from './Pages/Dashboard';
 import EmployeeList from './Pages/EmployeeList';
