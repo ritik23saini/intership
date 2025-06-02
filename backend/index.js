98d79df3c27be3790f1fe349dce6d73c88b7d1c6
ritik23saini <ritik23saini@gmail.com>
2025-06-02 18:05:42 +0530

    FRONTEND_URL

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const Port = process.env.PORT||8000;
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL,
   credentials: true,
 }));
 
